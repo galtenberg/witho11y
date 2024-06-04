87240ac0f1956db2ee1f6c76b51c47556f92913d
Christopher Galtenberg <galtenberg@us.ibm.com>
2024-06-04 15:56:26 -0700

    Code printing utility

---
+++ mdcode.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <directory>\n", os.Args[0])
+	}
+
+	root := os.Args[1]
+  err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() && info.Name() == ".git" {
+			return filepath.SkipDir
+		}
+		if info.Name() == "mdcode.go" || filepath.Ext(path) == ".mod" || filepath.Ext(path) == ".sum" {
+			return nil
+		}
+		if !info.IsDir() {
+			printFileAsMarkdown(path)
+		}
+		return nil
+	})
+
+  if err != nil {
+    log.Fatalf("Error walking the path %q: %v\n", root, err)
+  }
+}
+
+func printFileAsMarkdown(path string) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("Error reading file %s: %v\n", path, err)
+		return
+	}
+
+	fmt.Printf("## %s\n\n", path)
+	fmt.Printf("```\n%s\n```\n\n", string(content))
+}
